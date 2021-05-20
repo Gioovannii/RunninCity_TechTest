17ddced790f03a342e27677d20dcb9e4864df40b
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-05-20 10:26:07 +0800

    Initial Commit

---
+++ RunninCity_TechTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RunninCity_TechTest
+//
+//  Created by Giovanni Gaffé on 2021/5/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
