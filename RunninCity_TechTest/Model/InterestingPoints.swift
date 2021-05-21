e66cd5d07aff399d6a980e3e48d0759e5374ba0b
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-05-21 12:43:09 +0800

    Set an interesting Point conform to NSObject and who conform the the mk annotation

---
+++ RunninCity_TechTest/Model/InterestingPoints.swift
@@ -0,0 +1,36 @@
+//
+//  Annotation.swift
+//  RunninCity_TechTest
+//
+//  Created by Giovanni Gaffé on 2021/5/21.
+//
+
+import Foundation
+import MapKit
+
+class InterestingPoints: NSObject {
+
+    let name: String?
+    let location: CLLocation
+     
+    init(name: String?, longitude: Double, latitude: Double) {
+        self.name = name
+        self.location = CLLocation(latitude: latitude, longitude: longitude)
+        super.init()
+    }
+}
+
+extension InterestingPoints: MKAnnotation {
+    
+    var coordinate: CLLocationCoordinate2D {
+        get {
+            return location.coordinate
+        }
+    }
+    
+    var title: String? {
+        get {
+            return name
+        }
+    }
+}
