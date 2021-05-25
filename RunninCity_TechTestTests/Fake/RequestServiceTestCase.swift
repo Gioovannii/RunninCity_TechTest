258c619834d97a4c27b4bcb4286cfd30e00815b2
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-05-25 21:58:36 +0800

    Test requestService

---
+++ RunninCity_TechTestTests/Fake/RequestServiceTestCase.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkTestCase.swift
+//  RunninCity_TechTestTests
+//
+//  Created by Giovanni Gaffé on 2021/5/25.
+//
+
+@testable import RunninCity_TechTest
+import XCTest
+
+class RequestServiceTestCase: XCTestCase {
+    
+
+}
