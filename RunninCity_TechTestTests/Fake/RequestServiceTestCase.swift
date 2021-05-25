076629348f9bc150cd413a7e9e3ab9a2abcbe11f
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-05-25 21:59:18 +0800

    set expectation and start testing service

--- RunninCity_TechTestTests/Fake/RequestServiceTestCase.swift
+++ RunninCity_TechTestTests/Fake/RequestServiceTestCase.swift
@@ -10,5 +10,22 @@ import XCTest
 
 class RequestServiceTestCase: XCTestCase {
     
+    // MARK: - Error
+    
+    func testGetData_ShouldPostFailedCallbackError() {
+        let session = RequestService(session: URLSessionFake(data: nil, response: nil, error: FakeResponseData.error))
+        
+        let expectation = XCTestExpectation(description: "Wait for expectation")
+        
+        session.request { result in
+            switch result {
+            case .success(let data):
+                print(data)
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
+    }
 
 }
