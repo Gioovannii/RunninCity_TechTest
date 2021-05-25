b36f6337d70a652fb61c8a5910a32aa261e10111
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-05-25 21:52:34 +0800

    change target

--- RunninCity_TechTest/Supported Files/AppDelegate.swift
+++ RunninCity_TechTest/Supported Files/AppDelegate.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 @main
+@available(iOS 13.0, *)
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
