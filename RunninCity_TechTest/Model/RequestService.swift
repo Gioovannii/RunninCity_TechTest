2005c9ec8d01a45dc7d86fcef05871a7775541cb
Giovanni Gaffe <gaffejonathan@ymail.com>
2021-05-26 17:18:08 +0800

    use task to set data task

--- RunninCity_TechTest/Model/RequestService.swift
+++ RunninCity_TechTest/Model/RequestService.swift
@@ -18,7 +18,9 @@ final class RequestService {
     
     func request(callback: @escaping (Result<LmStudioJSON, NetworkError>) -> Void) {
         guard let baseUrl = URL(string: "http://lmstudio.free.fr/lyon.json") else { return }
-        let task = URLSession.shared.dataTask(with: baseUrl) { (data, response, error) in
+        task?.cancel()
+        task = session.dataTask(with: baseUrl) { (data, response, error) in
+
             guard let data = data, error == nil else {
                 print (error?.localizedDescription ?? "Response Error")
                 callback(.failure(.noData))
